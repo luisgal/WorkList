58d730a56b4ef93e79091af4c03e394fb3ede674
Luis Galindo <luisgalrod@gmail.com>
2023-05-20 14:44:59 -0600

    Primer commit - Aplicación Worklist, se crea la aplicación con Spring Boot, se agregan dependencias con Maven, se conecta con PostgreSQL y se hace uso de Liquibase para crear una primera tabla llamada Usuario.

---
+++ src/main/java/com/luisgal/project/worklist/WorkListApplication.java
@@ -0,0 +1,13 @@
+package com.luisgal.project.worklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisgal/project/worklist/WorkListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisgal.project.worklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
